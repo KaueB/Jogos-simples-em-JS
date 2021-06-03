e39599cb8d9e8f7413a6676a0e582739f74b1127
Kaue Batista <kauebatista.2004pm@gmail.com>
2021-06-03 09:25:03 -0300

    Coloquei " ; "

--- Jogo random/javascript.js
+++ Jogo random/javascript.js
@@ -1,31 +1,32 @@
 let alea = Math.floor(Math.random()* 100)
 let aleat = Number(alea.value)
 
-let reiniciar = document.getElementById('reiniciar') 
+let reiniciar = document.querySelector('input#reiniciar') 
 reiniciar.style.display = "none"
 
 function selecionar() {
-    let num = document.getElementById('num')
-    let res = document.getElementById('res')
+    let num = document.getElementById('num');
+    let res = document.getElementById('res');
 
     if (num.value.length == 0) {
-        alert('[ERRO] Esolha um número')        
+        alert('[ERRO] Esolha um número');
     } else if (alea < Number(num.value)) {
-        res.innerHTML = "Escolha um maior menor"
-        num.focus()
+        res.innerHTML = "Escolha um maior menor";
+        num.focus();
     } else if (alea > Number(num.value)) {
-        res.innerHTML = "Escolha um número maior"
-        num.focus()
+        res.innerHTML = "Escolha um número maior";
+        num.focus();
     } else {
-        let select = document.getElementById('select')
-        let parabens = document.getElementById('parabens')
-        select.innerHTML= ''
-        parabens.innerHTML = "Parabéns"
-        res.innerHTML = `Você acerto. O número sorteado foi ${alea}`
-        reiniciar.style.display = ""
-        function reiniciar() {
-            reload()
-        } 
+        let select = document.getElementById('select');
+        let parabens = document.getElementById('parabens');
+        select.innerHTML= '';
+        parabens.innerHTML = "Parabéns";
+        res.innerHTML = `Você acerto. O número sorteado foi ${alea}`;
+
+        reiniciar.style.display = "inline";
+        
     }
 }
-
+        function reiniciar() {
+            location.reload();
+        }
