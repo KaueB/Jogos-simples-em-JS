44e4e50e79af7e3934d289411dcdb92fb45a386b
Kaue Batista <kauebatista.2004pm@gmail.com>
2021-06-19 12:50:08 -0300

    tentamos

--- jogo random 2/script.js
+++ jogo random 2/script.js
@@ -1,7 +1,8 @@
-let resultadoNum = Math.floor(-100*Math.random()*100);
-let resultadoB1 = Math.floor(+100 * Math.random() - 100);
-let resultadoB2 = Math.floor(-100 * Math.random() + 100);
-let resultadoB3 = Math.floor(-100 * Math.random() + 100);
+let resultadoNum = Math.floor(Math.random() * 100);
+let resultadoB1 = Math.floor(Math.random() * 50);
+let resultadoB2 = Math.floor(Math.random() * 50);
+let resultadoB3 = Math.floor(Math.random() * -50);
+let resultadoB4 = Math.floor(Math.random() * -50);
 
 function load() {    
     resultado = document.getElementById('resultado');
@@ -9,35 +10,56 @@ function load() {
     document.getElementById('botao1').value = resultadoB1;
     document.getElementById('botao2').value = resultadoB2;
     document.getElementById('botao3').value = resultadoB3;
+    document.getElementById('botao4').value = resultadoB4
 }
 
 function botao1() {
-    resultadoNum = resultadoNum + resultadoB1
+    resultadoNum = resultadoNum + resultadoB3
     resultado.innerHTML = resultadoNum
-    resultadoB1 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB1 = Math.floor(Math.random() * 50);
     document.getElementById('botao1').value = resultadoB1
-    resultadoB2 = Math.floor(- 100 * Math.random() + 100);
+    resultadoB2 = Math.floor(Math.random() * 50);
     document.getElementById('botao2').value = resultadoB2
-    resultadoB3 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB3 = Math.floor(Math.random() * -50);
     document.getElementById('botao3').value = resultadoB3
+    resultadoB4 = Math.floor(Math.random() * -50);
+    document.getElementById('botao4').value = resultadoB4
 }
+
 function botao2() {
-    resultadoNum = resultadoNum + resultadoB2
+    resultadoNum = resultadoNum + resultadoB3
     resultado.innerHTML = resultadoNum
-    resultadoB1 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB1 = Math.floor(Math.random() * 50);
     document.getElementById('botao1').value = resultadoB1
-    resultadoB2 = Math.floor(- 100 * Math.random() + 100);
+    resultadoB2 = Math.floor(Math.random() * 50);
     document.getElementById('botao2').value = resultadoB2
-    resultadoB3 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB3 = Math.floor(Math.random() * -50);
     document.getElementById('botao3').value = resultadoB3
+    resultadoB4 = Math.floor(Math.random() * -50);
+    document.getElementById('botao4').value = resultadoB4
 } 
 function botao3() {
     resultadoNum = resultadoNum + resultadoB3
     resultado.innerHTML = resultadoNum
-    resultadoB1 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB1 = Math.floor(Math.random() * 50);
     document.getElementById('botao1').value = resultadoB1
-    resultadoB2 = Math.floor(- 100 * Math.random() +100);
+    resultadoB2 = Math.floor(Math.random() * 50);
     document.getElementById('botao2').value = resultadoB2
-    resultadoB3 = Math.floor(+ 100 * Math.random() - 100);
+    resultadoB3 = Math.floor(Math.random() * -50);
     document.getElementById('botao3').value = resultadoB3
+    resultadoB4 = Math.floor(Math.random() * -50);
+    document.getElementById('botao4').value = resultadoB4
+}
+
+function botao4() {
+    resultadoNum = resultadoNum + resultadoB3
+    resultado.innerHTML = resultadoNum
+    resultadoB1 = Math.floor(Math.random() * 50);
+    document.getElementById('botao1').value = resultadoB1
+    resultadoB2 = Math.floor(Math.random() * 50);
+    document.getElementById('botao2').value = resultadoB2
+    resultadoB3 = Math.floor(Math.random() * -50);
+    document.getElementById('botao3').value = resultadoB3
+    resultadoB4 = Math.floor(Math.random() * -50);
+    document.getElementById('botao4').value = resultadoB4
 }
